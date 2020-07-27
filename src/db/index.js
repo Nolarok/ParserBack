9f83e472168ef0d086d0684ac1c43f1d6c9bb3cc
Ivan Sharapov <Nolarok@users.noreply.github.com>
2020-07-27 10:10:49 +0300

    Proxy (throw fix)

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
 
 export async function dbConnect({host, port, name}) {
-  console.log(`DB connect string: mongodb://${host}:${port}`)
+  console.log(`DB connect string: mongodb://${host}:${port}. DB name: ${name}`)
 
   const connection = await mongoose.connect(`mongodb://${host}:${port}`, {
     dbName: name,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    user: 'root',
-    pass: 'qwAZ11',
+    // user: 'root',
+    // pass: 'qwAZ11',
   }).catch(error => {
     console.error(error)
   })
